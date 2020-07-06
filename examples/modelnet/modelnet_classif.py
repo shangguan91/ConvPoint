914a9d2f4584aa8e6bce791f0dadf7d03e6d2607
shangguan91 <64595972+shangguan91@users.noreply.github.com>
2020-07-06 18:57:13 +0200

    Update modelnet_classif.py

--- examples/modelnet/modelnet_classif.py
+++ examples/modelnet/modelnet_classif.py
@@ -20,74 +20,6 @@ import torch.utils.data
 
 import utils.metrics as metrics
 
-def get_data(rootdir, files):
-
-    train_filenames = []
-    for line in open(os.path.join(rootdir, files), "r"):
-        line = line.split("\n")[0]
-        line = os.path.basename(line)
-        train_filenames.append(os.path.join(rootdir, line))
-
-    data = []
-    labels = []
-    for filename in train_filenames:
-        f = h5py.File(filename, 'r')
-        data.append(f["data"])
-        labels.append(f["label"])
-
-    data = np.concatenate(data, axis=0)
-    labels = np.concatenate(labels, axis=0)
-
-    return data, labels
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-def pc_normalize(pc):
-    l = pc.shape[0]
-    centroid = np.mean(pc, axis=0)
-    pc = pc - centroid
-    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
-    pc = pc / m
-    return pc
-
-
-class PointCloudFileLists(torch.utils.data.Dataset):
-    """Main Class for Image Folder loader."""
-
-    def __init__(self, data, labels, pt_nbr=1024, training=True, num_iter_per_shape=1):
-        """Init function."""
-
-        self.data = data
-        self.labels = labels
-        self.training = training
-        self.pt_nbr = pt_nbr
-        self.num_iter_per_shape = num_iter_per_shape
-
-    def __getitem__(self, index):
-        """Get item."""
-
-
-        index_ = index//self.num_iter_per_shape
-
-        # get the filename
-        pts = self.data[index_]
-        target = self.labels[index_]
-
-
-        indices = np.random.choice(pts.shape[0], self.pt_nbr)
-        pts = pts[indices]
-
-        # create features
-        features = np.ones((pts.shape[0], 1))
-
-        pts = pc_normalize(pts)
-
-        return pts.astype(np.float32), features.astype(np.float32), int(target), index_
-
-    def __len__(self):
-        """Length."""
-        return self.data.shape[0] * self.num_iter_per_shape
 
 
 
@@ -114,19 +46,7 @@ def main():
     if args.cuda:
         torch.backends.cudnn.benchmark=True
 
-    # modelnet40
-    labels = [
-                "airplane", "bowl",     "desk",         "keyboard",     "person",
-                "sofa",     "tv_stand", "bathtub",      "car",          "door",
-                "lamp",     "piano",    "stairs",       "vase",         "bed",
-                "chair",    "dresser",  "laptop",       "plant",        "stool",
-                "wardrobe", "bench",    "cone",         "flower_pot",   "mantel",
-                "radio",    "table",    "xbox",         "bookshelf",    "cup",
-                "glass_box","monitor",  "range_hood",   "tent",         "bottle",
-                "curtain",  "guitar",   "night_stand",  "sink",         "toilet",
-                ]
-
-    # parameters for training
+    
     THREADS = 4
     N_LABELS = len(labels)
     epoch_nbr = 100
@@ -157,12 +77,6 @@ def main():
     print("done")
 
 
-    print("Creating dataloaders...", end="")
-    ds = PointCloudFileLists(train_data, train_labels, pt_nbr=args.npoints)
-    train_loader = torch.utils.data.DataLoader(ds, batch_size=args.batchsize, shuffle=True, num_workers=THREADS)
-    ds_test = PointCloudFileLists(test_data, test_labels, pt_nbr=args.npoints, training=False, num_iter_per_shape=args.ntree)
-    test_loader = torch.utils.data.DataLoader(ds_test, batch_size=args.batchsize, shuffle=False, num_workers=THREADS)
-    print("done")
 
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [30,60,90])
